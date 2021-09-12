e6a50d77a209fc01ca93eb856db31edae5f172af
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-09-12 08:56:18 -0500

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/python3
+""" Starts Flask. """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """ Hello HBNB!!! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ HBNB!!! """
+    return "HBNB"
+
+
+@app.route('/c/<phrase>', strict_slashes=False)
+def c(phrase):
+    """ C + Phrases!!! """
+    return 'C {}'.format(phrase.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run()
+© 2021 GitHub, Inc.
